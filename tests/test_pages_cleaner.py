e6a86a974cb70e5c034448bf6a47732791824492
Allle98 <47569637+Allle98@users.noreply.github.com>
2020-02-09 16:47:37 +0100

    Update test_pages_cleaner.py

--- tests/test_pages_cleaner.py
+++ tests/test_pages_cleaner.py
@@ -16,99 +16,146 @@ from os import listdir
 from os.path import isfile, join
 import os, io
 
-### _________FUNCTIONS______
-def findSquareBrackets(line):
-    i = 0
-    start = -1
-    end = -1
-    char = '['
-    flag = False
+class Cleaner:
+    def findSquareBrackets(self, line):
+        i = 0
+        start = -1
+        end = -1
+        char = '['
+        flag = False
 
-    while(i + 1 < len(line) and flag == False):
-        if(line[i] == char and line[i + 1] == char):
-            if char == '[':
-                start = i
-                char = ']'
-            else:
-                end = i + 1
-                flag = True
+        while(i + 1 < len(line) and flag == False):
+            if(line[i] == char and line[i + 1] == char):
+                if char == '[':
+                    start = i
+                    char = ']'
+                else:
+                    end = i + 1
+                    flag = True
 
-        i += 1
+            i += 1
 
-    return start, end
+        return start, end
 
-# input: substring, string between [[ ]] with brackets included
-def twoWordInSquareBrackets(line):
-    for c in line:
-        if c == '|':
-            return line.index(c)
+    # input: substring, string between [[ ]] with brackets included
+    def twoWordInSquareBrackets(self, line):
+        for c in line:
+            if c == '|':
+                return line.index(c)
 
-    return -1
+        return -1
 
-# input: substring, string between [[ ]] with brackets included
-def wordToReplace(line):
-    if line.startswith("[[File:") or line.startswith("[[Image:") or line.startswith("[[Category:"):
-        return ""
-    
-    index = twoWordInSquareBrackets(line)
+    # input: substring, string between [[ ]] with brackets included
+    def wordToReplace(self, line):
+        if line.startswith("[[File:") or line.startswith("[[Image:") or line.startswith("[[Category:"):
+            return ""
+        
+        index = self.twoWordInSquareBrackets(line)
 
-    if index == -1:
+        if index == -1:
+            return line[2 : -2]
+
+        else:
+            return line[index + 1 : -2]
+
+    def findTripleApostrophe(self, line):
+        i = 0
+        start = -1
+        end = -1
+        char = "'"
+        flag = False
+        flagStart = False
+
+        while(i + 2 < len(line) and flag == False):
+            if(line[i] == char and line[i + 1] == char and line[i + 2] == char):
+                if flagStart == False:            
+                    start = i
+                    flagStart = True
+
+                else:
+                    end = i + 3
+                    flag = True
+
+            i += 1
+
+        return start, end
+
+    def getWordTripleApostrophe(self, line):
+        return line[3 : -3]
+
+    def findDoubleApostrophe(self, line):
+        i = 0
+        start = -1
+        end = -1
+        char = "'"
+        flag = False
+        flagStart = False
+
+        while(i + 1 < len(line) and flag == False):
+            if(line[i] == char and line[i + 1] == char):
+                if flagStart == False:            
+                    start = i
+                    flagStart = True
+
+                else:
+                    end = i + 2
+                    flag = True
+
+            i += 1
+
+        return start, end
+
+    def getWordDoubleApostrophe(self, line):
         return line[2 : -2]
+    
+    def page_cleaner(self, text):
+        buff = io.StringIO(text)
+        #line = buf.readline()
+        data = ""
+        for line in buff:
+            start, end = self.findSquareBrackets(line)
+            startTA, endTA = self.findTripleApostrophe(line)
+            startDA, endDA = self.findDoubleApostrophe(line)
+            flag = False
+            
+            while((start != -1 and end != -1) or (startTA != -1 and endTA != -1) or (startDA != -1 and endDA != -1)):
+                 startTA, endTA = self.findTripleApostrophe(line)
+                 if startTA != -1 and endTA != -1:
+                    line = line.replace(line[startTA : endTA], self.getWordTripleApostrophe(line[startTA : endTA]))
+                    startTA, endTA = self.findTripleApostrophe(line)
 
-    else:
-        return line[index + 1 : -2]
+                 startDA, endDA = self.findDoubleApostrophe(line)
+                 if startDA != -1 and endDA != -1:
+                    line = line.replace(line[startDA : endDA], self.getWordDoubleApostrophe(line[startDA : endDA]))
+                    startDA, endDA = self.findDoubleApostrophe(line)
+                
+                 start, end = self.findSquareBrackets(line)
+                 if start != -1 and end != -1:
+                    word = line[start : end + 1]
+                    replace = self.wordToReplace(line[start : end + 1])
 
-def findTripleApostrophe(line):
-    i = 0
-    start = -1
-    end = -1
-    char = "'"
-    flag = False
-    flagStart = False
+                    if replace == "":
+                        flag = True
+                    line = line.replace(word, replace)
 
-    while(i + 2 < len(line) and flag == False):
-        if(line[i] == char and line[i + 1] == char and line[i + 2] == char):
-            if flagStart == False:            
-                start = i
-                flagStart = True
+                    start, end = self.findSquareBrackets(line)
+             
+            if(line == '\n') and flag == True:
+                flag = False
+            else:    
+                data += line 
 
-            else:
-                end = i + 3
-                flag = True
+        print(data)
+        return data  
 
-        i += 1
 
-    return start, end
-
-def getWordTripleApostrophe(line):
-    return line[3 : -3]
-
-def findDoubleApostrophe(line):
-    i = 0
-    start = -1
-    end = -1
-    char = "'"
-    flag = False
-    flagStart = False
-
-    while(i + 1 < len(line) and flag == False):
-        if(line[i] == char and line[i + 1] == char):
-            if flagStart == False:            
-                start = i
-                flagStart = True
-
-            else:
-                end = i + 2
-                flag = True
-
-        i += 1
-
-    return start, end
-
-def getWordDoubleApostrophe(line):
-    return line[2 : -2]
 # ____________
+#c = Cleaner()
 
+#fin = open("pages" + os.path.sep + "14.txt", "rt")
+#text = fin.read()
+#c.page_cleaner(text)
+'''
 folder = "pages"
 files = [f for f in listdir(folder) if isfile(join(folder, f))]
 
@@ -161,3 +208,4 @@ for page in files:
     fin = io.open(folder + os.path.sep + page, "wt",encoding="utf-8")
     fin.write(data)
     fin.close()
+'''
