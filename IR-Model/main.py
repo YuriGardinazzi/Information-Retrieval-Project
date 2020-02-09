21a0ecab8f0c452c71fdb7aba6c99ee79fb48d26
ggiacobazzi <gabrielegiacobazzi@hotmail.it>
2020-02-09 20:00:18 +0100

    extended menu

--- IR-Model/main.py
+++ IR-Model/main.py
@@ -7,6 +7,26 @@ Main file of the search-engine
 from dump_splitter import WikiSplitter
 from index_creator import Index
 
+def display_menu():
+    print("""
+          1. Create Index
+          2. Create Pages
+          3. Search something
+          4. Exit
+          """)
+    answer = input("What would you like to do? ")
+    if answer == "1":
+        create_index()
+    elif answer == "2":
+        split_files()
+    elif answer == "3":
+        query = str(input("Insert a term to search: "))
+        make_query(query)
+    elif answer == "4":
+        raise SystemExit
+    else:
+        print("Invalid choice")
+    
 def split_files():
     splitter = WikiSplitter()
     splitter.splitFiles()  
@@ -26,8 +46,5 @@ def make_query(text):
 
 if __name__ == "__main__":
     
-    response = str(input("Vuoi creare l'indice?[Y/N]"))
-    if response == 'Y':
-        create_index()
-    query = str(input("Cosa vuoi ricercare?"))
-    make_query(query)
+    while True:
+        display_menu()
