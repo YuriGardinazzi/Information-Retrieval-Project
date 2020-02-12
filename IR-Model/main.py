bf52ce84b79246ce9cdbb33c717383cdacb8db99
ggiacobazzi <gabrielegiacobazzi@hotmail.it>
2020-02-12 13:00:31 +0100

    updated with calc test

--- IR-Model/main.py
+++ IR-Model/main.py
@@ -6,13 +6,14 @@ Main file of the search-engine
 
 from dump_splitter import WikiSplitter
 from index_creator import Index
+import calc as Calculator
 
 def display_menu():
     print("""
           1. Create Index
           2. Create Pages
           3. Search something
-          4. Exit
+2          4. Exit
           """)
     answer = input("What would you like to do? ")
     if answer == "1":
@@ -42,8 +43,9 @@ def make_query(text):
         for x in result:
             print("*********************")
             print(x['title'])
-            print(x['textdata'][:500])
-
+            print(x['textdata'][:3000])
+            print(Calculator.findWordInQuery(text, x))
+            
 if __name__ == "__main__":
     
     while True:
