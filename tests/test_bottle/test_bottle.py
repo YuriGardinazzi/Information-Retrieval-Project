e15fcc3dfad68d4e30dbccddd3c2b2b9ae4743f9
Yuri Gardinazzi <gardinazzi.yuri@gmail.com>
2020-02-13 16:58:01 +0100

    Add error 404 handling

--- tests/test_bottle/test_bottle.py
+++ tests/test_bottle/test_bottle.py
@@ -3,10 +3,13 @@
 Hello world of a webpage made with bottle
 """
 
-from bottle import route, run, template
+from bottle import route, run, error
 
 @route('/')
 def index():
     return '<h1>Hello WORLD!</h1>'
 
+@error(404)
+def error404(error):
+    return 'Nothing here, sorry'
 run(host='localhost', port=8080)
