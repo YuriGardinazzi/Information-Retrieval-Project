60b7312ef6485f3c774f693ca8f7ae2963bb388f
Allle98 <47569637+Allle98@users.noreply.github.com>
2020-02-14 13:53:41 +0100

    Update cleaner.py

--- IR-Model/cleaner.py
+++ IR-Model/cleaner.py
@@ -8,22 +8,58 @@ from os.path import isfile, join
 import os, io
 
 class Cleaner:
-    def findSquareBrackets(self, line):
+    def findDoubleSign(self, line, simbleStart, simbleEnd):
         i = 0
         start = -1
         end = -1
-        char = '['
+        char = simbleStart
         flag = False
+        startFind = False
 
         while(i + 1 < len(line) and flag == False):
             if(line[i] == char and line[i + 1] == char):
-                if char == '[':
+                if char == simbleStart and startFind == False:
                     start = i
-                    char = ']'
-                else:
-                    end = i + 1
+                    char = simbleEnd
+                    startFind = True
+                
+                elif char == simbleEnd and startFind == True:
+                    end = i + 2
                     flag = True
 
+            i += 1
+
+        return start, end
+
+    def findSquareBrackets(self, line, simbleStart, simbleEnd):
+        i = 0
+        start = -1
+        end = -1
+        char = simbleStart
+        flag = False
+        nested = False
+        startFind = False
+
+        
+        if line.startswith("[[File:") or line.startswith("[[Image:") or line.startswith("[[Category:"):
+            return -2, -2
+
+        while(i + 1 < len(line) and flag == False):
+            if(line[i] == char and line[i + 1] == char):
+                if char == simbleStart:
+                    if startFind == False:
+                        start = i
+                        char = simbleEnd
+                        startFind = True
+                    else:
+                        nested = True   
+                else:
+                    if char == simbleEnd and startFind == True and nested == False:
+                        end = i + 2
+                        flag = True
+                    else: 
+                        if char == simbleEnd and nested == True:
+                            nested = False
             i += 1
 
         return start, end
@@ -38,9 +74,6 @@ class Cleaner:
 
     # input: substring, string between [[ ]] with brackets included
     def wordToReplace(self, line):
-        if line.startswith("[[File:") or line.startswith("[[Image:") or line.startswith("[[Category:"):
-            return ""
-        
         index = self.twoWordInSquareBrackets(line)
 
         if index == -1:
@@ -73,31 +106,10 @@ class Cleaner:
 
     def getWordTripleApostrophe(self, line):
         return line[3 : -3]
-
-    def findDoubleApostrophe(self, line):
-        i = 0
-        start = -1
-        end = -1
-        char = "'"
-        flag = False
-        flagStart = False
-
-        while(i + 1 < len(line) and flag == False):
-            if(line[i] == char and line[i + 1] == char):
-                if flagStart == False:            
-                    start = i
-                    flagStart = True
-
-                else:
-                    end = i + 2
-                    flag = True
-
-            i += 1
-
-        return start, end
-
-    def getWordDoubleApostrophe(self, line):
-        return line[2 : -2]
+    
+    def getWordDoubleSign(self, line):
+        return line[2 : -2]       
+        
     
     def page_cleaner(self, text):
 
@@ -105,37 +117,70 @@ class Cleaner:
 
         data = ""
         for line in buff:
-            start, end = self.findSquareBrackets(line)
+            if line[0] == '|' or (line[0] == ' ' and line[1] == '|'):
+                line = ""
+            start, end = self.findSquareBrackets(line, '[', ']')
             startTA, endTA = self.findTripleApostrophe(line)
-            startDA, endDA = self.findDoubleApostrophe(line)
+            startDA, endDA = self.findDoubleSign(line, "'", "'")
+            startEq, endEq = self.findDoubleSign(line, '=', '=')
+            startBr, endBr = self.findDoubleSign(line, '{', '}')
             flag = False
             
-            while((start != -1 and end != -1) or (startTA != -1 and endTA != -1) or (startDA != -1 and endDA != -1)):
-                 startTA, endTA = self.findTripleApostrophe(line)
-                 if startTA != -1 and endTA != -1:
-                    line = line.replace(line[startTA : endTA], self.getWordTripleApostrophe(line[startTA : endTA]))
-                    startTA, endTA = self.findTripleApostrophe(line)
-
-                 startDA, endDA = self.findDoubleApostrophe(line)
-                 if startDA != -1 and endDA != -1:
-                    line = line.replace(line[startDA : endDA], self.getWordDoubleApostrophe(line[startDA : endDA]))
-                    startDA, endDA = self.findDoubleApostrophe(line)
+            while((start != -1 and end != -1) or (startTA != -1 and endTA != -1) \
+                or (startDA != -1 and endDA != -1) or (startEq != -1 and endEq != -1) or (startBr != -1 and endBr != -1)):
                 
-                 start, end = self.findSquareBrackets(line)
-                 if start != -1 and end != -1:
-                    word = line[start : end + 1]
-                    replace = self.wordToReplace(line[start : end + 1])
-
+                start, end = self.findSquareBrackets(line, '[', ']')
+                if start == -2 and end == -2:
+                    line = line.replace(line, "")
+                    start, end = self.findSquareBrackets(line, '[', ']') 
+                elif start != -1 and end != -1:
+                    word = line[start : end]
+                    replace = self.wordToReplace(line[start : end])
                     if replace == "":
                         flag = True
                     line = line.replace(word, replace)
 
-                    start, end = self.findSquareBrackets(line)
+                    start, end = self.findSquareBrackets(line, '[', ']')                
+                
+                startTA, endTA = self.findTripleApostrophe(line)
+                if startTA != -1 and endTA != -1:
+                    print(line[startTA : endTA])
+                    print(self.getWordTripleApostrophe(line[startTA : endTA]))
+                    line = line.replace(line[startTA : endTA], self.getWordTripleApostrophe(line[startTA : endTA]))
+                    startTA, endTA = self.findTripleApostrophe(line)
+                    print("LINE " + line)
+                
+                
+                startDA, endDA = self.findDoubleSign(line, "'", "'")
+                if startDA != -1 and endDA != -1:
+                    line = line.replace(line[startDA : endDA], self.getWordDoubleSign(line[startDA : endDA]))
+                    print(self.getWordDoubleSign(line[startDA : endDA]))
+                    startDA, endDA = self.findDoubleSign(line, "'", "'")
+
+                startEq, endEq = self.findDoubleSign(line, '=', '=')
+                if startEq != -1 and endEq != -1:
+                    line = line.replace(line[startEq : endEq], self.getWordDoubleSign(line[startEq : endEq]))
+                    startEq, endEq = self.findDoubleSign(line, '=', '=')
+
+                startBr, endBr = self.findDoubleSign(line, '{', '}')
+                if startBr != -1 and endBr != -1:
+                    print(line[startBr : endBr])
+                    line = line.replace(line[startBr : endBr], "")
+                    startBr, endBr = self.findDoubleSign(line, '{', '}')
              
-            if(line == '\n') and flag == True:
+            if(line == '\n'):  # tolto and flag == True per togliere le righe vuote
                 flag = False
-            else:    
-                data += line 
-
-        return data  
-
+            elif line != '\n' and line != "*\n" and line != "* \n":    
+                data += line
+        
+        data = data.replace("()", "")
+        data = data.replace("( )", "")
+        data = data.replace("===", "")
+        # Per togliere {{ }} su più righe
+        startBr, endBr = self.findDoubleSign(data, '{', '}')
+        while(startBr != -1 and endBr != -1):
+            print(data[startBr : endBr])
+            data = data.replace(data[startBr : endBr], "")
+            startBr, endBr = self.findDoubleSign(data, '{', '}')
+          
+        return data
