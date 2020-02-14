9eab4d43f01b7163227ce5f656a918390f35863b
Allle98 <47569637+Allle98@users.noreply.github.com>
2020-02-14 20:24:39 +0100

    Add files via upload

---
+++ IR-Model/make_csv.py
@@ -0,0 +1,62 @@
+from statistics import get_google_ranking
+from main import get_title_result
+import math
+
+SEPARATOR = ";"
+
+def calculate_statistics():
+    dataCSV = "QUERY" + SEPARATOR + "AVERAGE_PRECISION_QUERY" + SEPARATOR + "NDCG\n"
+    dict_google_results = get_google_ranking()
+    MAP = 0
+
+    for key in dict_google_results:
+        average_precision_query = 0
+        contRelevant = 0 
+        sumRelevance = 0
+        relevance = []
+
+        list_titles = get_title_result(key)
+        list_titles = [x[:-1] for x in list_titles[0]]
+
+        list_google_titles = [x for x,_ in dict_google_results[key]]
+        relevance_google =  [x for _,x in dict_google_results[key]]
+ 
+        for i in range(len(list_titles)):
+            if list_titles[i] in list_google_titles:
+                contRelevant += 1
+                #precision.append(contRelevant / i)
+                sumRelevance += (contRelevant / (i + 1))
+                relevance.append(relevance_google[list_google_titles.index(list_titles[i])])
+            else:
+                relevance.append(0)
+        if contRelevant != 0:
+            average_precision_query = sumRelevance / contRelevant
+        
+        # Calcolo DCG e iDCG
+        ordered_relevance = relevance.copy()
+        ordered_relevance.sort(reverse = True)
+
+        
+        j = 2
+        DCG = relevance[0]
+        iDCG = ordered_relevance[0]
+        while j - 1 < len(relevance):
+            DCG += relevance[j - 1] / math.log2(j)
+            iDCG += ordered_relevance[j - 1] / math.log2(j);
+            j += 1
+
+        NDCG = 0
+        if iDCG != 0:
+            NDCG = DCG / iDCG
+
+        dataCSV += key + SEPARATOR + str(average_precision_query) + SEPARATOR + str(NDCG) + '\n'
+        MAP += average_precision_query
+        
+    MAP /= len(dict_google_results)
+    dataCSV += '\n\n'
+    dataCSV += "MAP" + SEPARATOR + str(MAP) + '\n'
+    with open("statistics.csv", 'w', encoding='utf-8') as f:
+        f.write(dataCSV)
+
+if __name__ == "__main__":
+    calculate_statistics()
