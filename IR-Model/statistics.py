237d678fcbb4cf446c05d305b2de42303c40b3cf
Yuri Gardinazzi <gardinazzi.yuri@gmail.com>
2020-02-14 15:47:47 +0100

    Add get google ranking function

--- IR-Model/statistics.py
+++ IR-Model/statistics.py
@@ -6,39 +6,38 @@ import simplejson as json
 from os import listdir
 from os.path import isfile, join
 import os
+import io
 def get_google_ranking():
-    
-    path = "google_results"
-    
-    #struttura {()}
+    """This function returns a dictionary where 
+    key = Title
+    value = List of the first 10 results"""
     res = {}
-    files = [f for f in listdir(path) if isfile(join(path, f))]
-    for f in files:
-        title_list = []
-        filepath = os.path.join(path, f)
-        with open(filepath,"r") as jsonFile:
-            data = json.load(jsonFile)
-            print (data['link'])
-        
-def try_read():
     table = []
     path="google_results"
     files = [f for f in listdir(path) if isfile(join(path, f))]
     for f in files:
         filepath = os.path.join("google_results",f)
-        with open(filepath, 'r') as f:
-            for line in f:
+        #f[:-5] removes the .json extension
+        res[f[:-5]] = []
+        with io.open(filepath, 'r',encoding="utf-8") as file_json:
+            for line in file_json:
                 try:
-                    j = line.split('|')[-1]
-                    table.append(json.loads(j))
+                    data = json.loads(line)
+                    titolo= data['title'].replace(" - Wikipedia","")             
+                    res[f[:-5]].append(str(titolo))
+                   
+                    table.append(data)
                 except ValueError:
                     # You probably have bad JSON
                     continue
     
-    for row in table:
-        print(row['title'])
-    
+    #for row in table:
+     #   print(row['title'].replace(" - Wikipedia",""))
+    return res
 if __name__ == "__main__":
     print("Statistic:")
-    try_read()
+    res = get_google_ranking()
+    for key in res.keys():
+        print("Key: ", key,"num_risultati: ",len(res[key]))
+
    # get_google_ranking()
