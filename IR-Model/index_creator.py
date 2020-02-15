48e8a43f51d68704ff35aca7bc116404f58779b8
ggiacobazzi <gabrielegiacobazzi@hotmail.it>
2020-02-15 17:38:25 +0100

    updated with query-expansion

--- IR-Model/index_creator.py
+++ IR-Model/index_creator.py
@@ -7,9 +7,6 @@ Created on Sun Feb  9 18:24:22 2020
 
 from whoosh.index import create_in, open_dir
 from whoosh.fields import *
-from whoosh import analysis
-from whoosh import query as queryy
-from whoosh.formats import Frequency
 from whoosh.qparser import QueryParser
 from whoosh.scoring import WeightingModel
 from whoosh.scoring import Weighting
@@ -17,6 +14,7 @@ from whoosh.scoring import PL2
 
 from math import log
 
+import query_expander as expander
 import os, os.path, io, shutil
 
 class Cosine(Weighting):
@@ -42,17 +40,11 @@ class Index:
         #creation of the schema
         self.directory = index_directory
         self.files_path = input_files
-        #self.schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT(), textdata=TEXT(stored=True))
-        #analyzer = analysis.StandardAnalyzer(stoplist = frozenset(['and', 'is', 'it', 'an', 'as', 'at', 'have', 'in', 'yet', 'if', 'from', 'for', 'when', 'by', 'to', 'you', 'be', 'we', 'that', 'may', 'not', 'with', 'tbd', 'a', 'on', 'your', 'this', 'of', 'us', 'will', 'can', 'the', 'or', 'are']))
-        analyzer = analysis.StemmingAnalyzer()
-        vector_format = Frequency()
-        self.schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT(analyzer=analyzer, vector=vector_format, stored=True), links = STORED, textdata=TEXT(stored=True))
+        self.schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT, textdata=TEXT(stored=True))
    
     def createSchema(self): #aggiungere parametri per schemi personalizzati
         '''create a schema for the index'''
-        analyzer = StandardAnalyzer(stoplist=ENGLISH_STOP_WORDS)
-        vector_format = formats.Frequency(analyzer)
-        self.schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT(analyzer=analyzer, vector=vector_format), links = STORED, textdata=TEXT(stored=True))
+        self.schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT, textdata=TEXT(stored=True))
         
     def createIndex(self):
         ''' Create an index and delete existing ones'''
@@ -105,33 +97,20 @@ class Index:
         corrected = searcher.correct_query(query, input_query)
         if corrected.query != query:
              print("Did you mean:", corrected.string)
-             results = searcher.search(corrected.query, limit = 10)
-        else:
-            results = searcher.search(query, limit = 10)
+             query = corrected.query
+        results = searcher.search(query, limit = 10)
         
         #Query-expansion (blank return atm)
         if expanse_val:
-            number = int(input("Which document do you want to expand? "))
-            docnum = results.docnum(number)
-            key_terms = list(searcher.key_terms([docnum], "content", numterms=5))
-            #print key_terms (debug purposes)
-            for t in key_terms:
-                print(t)
-        
-            """
-            problem lines imo
-            """
-            new_query = queryy.Or([queryy.Term("content", t) for t in key_terms])
-            results = searcher.search(new_query, limit = 10)
+            new_query = str(expander.queryExpander(input_query))
+            query_expanded = parser.parse(new_query)  
+            print("Nuova query: " + new_query)
+            results = searcher.search(query_expanded, limit = 10)
             
-            #first_hit = results[0]
-            #more_results = first_hit.more_like_this("content")
-            #return more_results
-        else:  
-            if len(results) == 0:
-                print("Empty Result")
-                return None
-            else:
-                return results
+        if len(results) == 0:
+            print("Empty Result")
+            return None
+        else:
+            return results
             
         
